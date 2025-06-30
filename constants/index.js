1a01fa885e8020236ee7854819e9cf5dc46c2d55
Ujjwal Jha <findingaman25@gmail.com>
2025-06-30 17:23:02 +0530

    GSAP animation landing page

---
+++ constants/index.js
@@ -0,0 +1,178 @@
+const navLinks = [
+ {
+	id: "cocktails",
+	title: "Cocktails",
+ },
+ {
+	id: "about",
+	title: "About Us",
+ },
+ {
+	id: "work",
+	title: "The Art",
+ },
+ {
+	id: "contact",
+	title: "Contact",
+ },
+];
+
+const cocktailLists = [
+ {
+	name: "Chapel Hill Shiraz",
+	country: "AU",
+	detail: "Battle",
+	price: "₹180",
+ },
+ {
+	name: "Caten Malbee",
+	country: "AU",
+	detail: "Battle",
+	price: "₹149",
+ },
+ {
+	name: "Rhino Pale Ale",
+	country: "CA",
+	detail: "750 ml",
+	price: "₹210",
+ },
+ {
+	name: "Irish Guinness",
+	country: "IE",
+	detail: "600 ml",
+	price: "₹290",
+ },
+];
+
+const mockTailLists = [
+ {
+	name: "Tropical Bloom",
+	country: "US",
+	detail: "Battle",
+	price: "₹170",
+ },
+ {
+	name: "Passionfruit Mint",
+	country: "US",
+	detail: "Battle",
+	price: "₹149",
+ },
+ {
+	name: "Citrus Glow",
+	country: "CA",
+	detail: "750 ml",
+	price: "₹210",
+ },
+ {
+	name: "Lavender Fizz",
+	country: "IE",
+	detail: "600 ml",
+	price: "₹290",
+ },
+];
+
+const profileLists = [
+ {
+	imgPath: "/images/profile1.png",
+ },
+ {
+	imgPath: "/images/profile2.png",
+ },
+ {
+	imgPath: "/images/profile3.png",
+ },
+ {
+	imgPath: "/images/profile4.png",
+ },
+];
+
+const featureLists = [
+ "Perfectly balanced blends",
+ "Garnished to perfection",
+ "Ice-cold every time",
+ "Expertly shaken & stirred",
+];
+
+const goodLists = [
+ "Handpicked ingredients",
+ "Signature techniques",
+ "Bartending artistry in action",
+ "Freshly muddled flavors",
+];
+
+const storeInfo = {
+ heading: "Where to Find Us",
+ address: "Bengaluru, Karnataka, India - 560111",
+ contact: {
+	phone: "+91 1234567890",
+	email: "searchingforujjwal@gmail.com",
+ },
+};
+
+const openingHours = [
+ { day: "Mon–Thu", time: "11:00am – 12am" },
+ { day: "Fri", time: "11:00am – 2am" },
+ { day: "Sat", time: "9:00am – 2am" },
+ { day: "Sun", time: "9:00am – 1am" },
+];
+
+const socials = [
+ {
+	name: "GitHub",
+	icon: "/images/github.png",
+	url: "https://github.com/i-ujjwaljha",
+ },
+ {
+	name: "LinkedIn",
+	icon: "/images/linkedin.png",
+	url: "https://www.linkedin.com/in/i-ujjwaljha/",
+ },
+];
+
+const allCocktails = [
+ {
+	id: 1,
+	name: "Classic Mojito",
+	image: "/images/drink1.png",
+	title: "Simple Ingredients, Bold Flavor",
+	description:
+	 "Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
+ },
+ {
+	id: 2,
+	name: "Raspberry Mojito",
+	image: "/images/drink2.png",
+	title: "A Zesty Classic That Never Fails",
+	description:
+	 "The Margarita is a classic that balances tangy lime, smooth tequila, and a touch of sweetness. Shaken, frozen, or on the rocks—it’s always crisp & refreshing.",
+ },
+ {
+	id: 3,
+	name: "Violet Breeze",
+	image: "/images/drink3.png",
+	title: "Simple Ingredients, Bold Flavor",
+	description:
+	 "Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
+ },
+ {
+	id: 4,
+	name: "Curacao Mojito",
+	image: "/images/drink4.png",
+	title: "Crafted With Care, Poured With Love",
+	description:
+	 "Each cocktail is made with fresh ingredients and a passion for perfecting every pour, whether you're celebrating or simply relaxing.",
+ },
+];
+
+export {
+ navLinks,
+ cocktailLists,
+ mockTailLists,
+ profileLists,
+ featureLists,
+ goodLists,
+ openingHours,
+ storeInfo,
+ socials,
+ allCocktails,
+};
